433479e041db7a99ebfe3d20ccdf7f5d8b629c6c
Ahmed Abbasi <ahmed-abbasi@outlook.com>
2023-03-08 21:34:00 +0100

    fixed for loop when updating score

--- src/scoreboard/scoreboard.py
+++ src/scoreboard/scoreboard.py
@@ -14,7 +14,7 @@ class Scoreboard:
 
     def update_score(self, name, score):
         """Add a new score to the list."""
-        for i in enumerate(len(self.scores)):
+        for i in enumerate(self.scores):
             if name in self.scores[i][0]:
                 self.scores[i][1] += score
 
