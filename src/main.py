efe5c8f7b3bc555c7f89bdec305fa2be05c9aed2
Nour <117777073+MoNourH@users.noreply.github.com>
2023-03-08 18:23:15 +0100

    updated dice + terminal and added main

--- src/main.py
+++ src/main.py
@@ -21,11 +21,51 @@ class Main(Cmd):
 
         scoreboard = Scoreboard([player1.get_name(), player2.get_name()])
         terminal = Terminal()
+        dice = Dice()
+        
+        tracker = 1
         while not scoreboard.get_winner():
-            terminal.display_clear()
+            # terminal.display_clear()
             terminal.display_table(scoreboard.get_scores())
             terminal.display_realtime_menu()
-            break
+
+            while True:
+                try:
+                    choice = int(input("Choose an option > "))
+                    if not 0 < choice < 5:
+                        raise ValueError
+                    break
+                except ValueError:
+                    print("Invalid option!")
+                    continue
+
+            match(choice):
+                case 1:
+                    print(tracker)
+                    result = dice.roll([1, 2, 3, 4, 5, 6])
+                    terminal.display_dice(result['cast'])
+                    if tracker == 1:
+                        if 1 in result['cast']:
+                            scoreboard.reset_score(player1.get_name())
+                        else:
+                            scoreboard.update_score(player1.get_name(), result['sum'])
+                    else:
+                        if 1 in result['cast']:
+                            scoreboard.reset_score(player2.get_name())
+                        else:
+                            scoreboard.update_score(player2.get_name(), result['sum'])
+                case 2:
+                    if tracker == 1:
+                        tracker = 2
+                        
+                    else:
+                        tracker = 1
+                        
+                case 3:
+                    print("3")
+                case 4:
+                    print("4")
+
 
 
 if __name__ == '__main__':
