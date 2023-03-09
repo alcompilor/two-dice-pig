1e815d835f2776bc25a181be4bf4bd2ccad272fd
Ahmed Abbasi <ahmed-abbasi@outlook.com>
2023-03-09 17:15:29 +0100

    improved code quality

--- src/main.py
+++ src/main.py
@@ -14,6 +14,9 @@ from scoreboard.scoreboard import Scoreboard
 from terminal.terminal import Terminal
 
 
+terminal = Terminal()
+
+
 class Main(Cmd):
     """Main class."""
 
@@ -21,28 +24,17 @@ class Main(Cmd):
 
     def preloop(self):
         """Run after starting cmdloop."""
-        terminal = Terminal()
-        terminal.display_clear()
-        terminal.display_title()
-        terminal.display_intro_img()
-        print("Welcome to Two-Pig Dice, where challenges abound!\n")
-        terminal.display_intro_menu()
-        print("Type 'help' to see available commands.\n")
+        welcome()
 
     def do_multiplayer(self, _):
         """Command to invoke a player vs player game."""
-        player1 = str(input("Enter your name (Player 1): "))
-        player2 = str(input("Enter your name (Player 2): "))
-        player1 = Player(player1, False)
-        player2 = Player(player2, False)
-
-        scoreboard = Scoreboard([player1.get_name(), player2.get_name()])
-        terminal = Terminal()
-        dice = Dice()
+        player1, player2 = create_players(True)
+        scoreboard, dice = init_game(player1, player2)
 
         terminal.display_clear()
+
         tracker = 1
-        while not scoreboard.get_winner():
+        while not scoreboard.get_winner():  # game round loop
             # terminal.display_clear()
             terminal.display_table(scoreboard.get_scores())
             terminal.display_realtime_menu()
@@ -52,51 +44,48 @@ class Main(Cmd):
             else:
                 print(f"It's {player2.get_name()}'s turn\n")
 
-            while True:
-                try:
-                    choice = int(input("Choose an option > "))
-                    if not 0 < choice < 5:
-                        raise ValueError
-                    break
-                except ValueError:
-                    print("Invalid option!")
-                    continue
+            choice = validate("Choose an option > ", "Invalid option!")
 
             match(choice):
                 case 1:
                     terminal.display_clear()
-                    result = dice.roll([1, 2, 3, 4, 5, 6])
-                    terminal.display_dice(result['cast'])
+
+                    cast, sum_score = dice.roll([1, 2, 3, 4, 5, 6]).values()
+                    terminal.display_dice(cast)
+
                     if tracker == 1:
-                        if result['cast'][0] == 1 and result['cast'][1] == 1:
+
+                        if (cast[0] == 1) and (cast[1] == 1):
                             scoreboard.reset_score(player1.get_name())
                             tracker = 2
                             continue
-                        elif 1 in result['cast']:
+                        if (cast[0] == 1) or (cast[1] == 1):
                             tracker = 2
                             continue
-                        else:
-                            scoreboard.update_score(
-                                player1.get_name(), result['sum'])
+
+                        scoreboard.update_score(
+                            player1.get_name(), sum_score)
                     else:
-                        if result['cast'][0] == 1 and result['cast'][1] == 1:
+
+                        if (cast[0] == 1) and (cast[1] == 1):
                             scoreboard.reset_score(player2.get_name())
                             tracker = 1
                             continue
-                        elif 1 in result['cast']:
+                        if (cast[0] == 1) or (cast[1] == 1):
                             tracker = 1
                             continue
-                        else:
-                            scoreboard.update_score(
-                                player2.get_name(), result['sum'])
+
+                        scoreboard.update_score(
+                            player2.get_name(), sum_score)
                 case 2:
                     terminal.display_clear()
                     if tracker == 1:
                         tracker = 2
                         continue
-                    else:
-                        tracker = 1
-                        continue
+
+                    tracker = 1
+                    continue
+
                 case 3:
                     new_name = str(input("Enter a new name: "))
                     if tracker == 1:
@@ -106,41 +95,23 @@ class Main(Cmd):
                 case 4:
                     break
 
-        if scoreboard.get_winner():
-            terminal.display_clear()
-            terminal.display_winner(scoreboard.get_winner())
-            terminal.display_table(scoreboard.get_scores())
+        end_game(scoreboard)
 
     def do_bot(self, _):
         """Command to invoke player vs bot game."""
-        player1 = str(input("Enter your name: "))
-        player2 = random.choice(
-            ["Alex (Bot)", "Nadia (Bot)", "Alice (Bot)", "John (Bot)"])
-        player1 = Player(player1, False)
-        player2 = Player(player2, True)
-
-        scoreboard = Scoreboard([player1.get_name(), player2.get_name()])
-        terminal = Terminal()
-        dice = Dice()
+        player1, player2 = create_players(False)
+        scoreboard, dice = init_game(player1, player2)
 
         terminal.display_clear()
-        difficulty = None
-        while True:
-            try:
-                terminal.display_computer_menu()
-                difficulty = int(input("Choose bot difficulty > "))
-                if not 0 < difficulty < 4:
-                    raise ValueError
-                break
-            except ValueError:
-                print("Invalid option!")
-                continue
+
+        terminal.display_computer_menu()
+        difficulty = validate("Choose bot difficulty > ", "Invalid option!")
 
         computer = Computer(difficulty)
-
         terminal.display_clear()
+
         tracker = 1
-        while not scoreboard.get_winner():
+        while not scoreboard.get_winner():  # game round loop
             terminal.display_table(scoreboard.get_scores())
             terminal.display_realtime_menu()
 
@@ -182,58 +153,46 @@ class Main(Cmd):
                     continue
             else:
                 print("It's your turn!\n")
-                while True:
-                    try:
-                        choice = int(input("Choose an option > "))
-                        if not 0 < choice < 5:
-                            raise ValueError
+                choice = validate("Choose an option > ", "Invalid option!")
+
+                match(choice):
+                    case 1:
+                        terminal.display_clear()
+                        result = dice.roll([1, 2, 3, 4, 5, 6])
+                        terminal.display_dice(result['cast'])
+                        if tracker == 1:
+                            if result['cast'][0] == 1 and result['cast'][1] == 1:
+                                scoreboard.reset_score(player1.get_name())
+                                print("You lost your entire score :(")
+                                tracker = 2
+                                time.sleep(4)
+                                continue
+                            elif 1 in result['cast']:
+                                print("You lost your turn and its score :/")
+                                tracker = 2
+                                time.sleep(4)
+                                continue
+                            else:
+                                scoreboard.update_score(
+                                    player1.get_name(), result['sum'])
+                    case 2:
+                        if tracker == 1:
+                            tracker = 2
+                            continue
+                    case 3:
+                        new_name = str(input("Enter a new name: "))
+                        if tracker == 1:
+                            player1.set_name(new_name)
+                    case 4:
                         break
-                    except ValueError:
-                        print("Invalid option!")
+
+                    case _:
                         continue
 
-            match(choice):
-                case 1:
-                    terminal.display_clear()
-                    result = dice.roll([1, 2, 3, 4, 5, 6])
-                    terminal.display_dice(result['cast'])
-                    if tracker == 1:
-                        if result['cast'][0] == 1 and result['cast'][1] == 1:
-                            scoreboard.reset_score(player1.get_name())
-                            print("You lost your entire score :(")
-                            tracker = 2
-                            time.sleep(4)
-                            continue
-                        elif 1 in result['cast']:
-                            print("You lost your turn and its score :/")
-                            tracker = 2
-                            time.sleep(4)
-                            continue
-                        else:
-                            scoreboard.update_score(
-                                player1.get_name(), result['sum'])
-                case 2:
-                    if tracker == 1:
-                        tracker = 2
-                        continue
-                case 3:
-                    new_name = str(input("Enter a new name: "))
-                    if tracker == 1:
-                        player1.set_name(new_name)
-                case 4:
-                    break
-
-                case _:
-                    continue
-
-        if scoreboard.get_winner():
-            terminal.display_clear()
-            terminal.display_winner(scoreboard.get_winner())
-            terminal.display_table(scoreboard.get_scores())
+        end_game(scoreboard)
 
     def do_rules(self, _):
         """Command to display game rules."""
-        terminal = Terminal()
         terminal.display_rules()
 
     def do_credits(self, _):
@@ -249,6 +208,76 @@ class Main(Cmd):
         """Command to exit game."""
         print("See you later!\n")
         return True
+
+
+def welcome():
+    """Display game intrro (welcome)."""
+    terminal.display_clear()
+    terminal.display_title()
+    terminal.display_intro_img()
+    print("Welcome to Two-Pig Dice, where challenges abound!\n")
+    terminal.display_intro_menu()
+    print("Type 'help' to see available commands.\n")
+
+
+def validate(prompt, error):
+    """Validate inputted int."""
+    while True:
+        try:
+            choice = int(input(prompt))
+            if "difficulty" not in prompt:
+                if not 0 < choice < 5:
+                    raise ValueError
+                return choice
+
+            if not 0 < choice < 4:
+                raise ValueError
+            return choice
+
+        except ValueError:
+            print(error)
+            continue
+
+
+def create_players(is_multiplayer):
+    """Create players for the game."""
+    if is_multiplayer:
+        player1_name = str(input("Enter your name (Player 1): "))
+
+        while True:
+            player2_name = str(input("Enter your name (Player 2): "))
+            if player1_name == player2_name:
+                print("You can't have the same name as Player 1")
+                continue
+            break
+
+        player1 = Player(player1_name, False)
+        player2 = Player(player2_name, False)
+        return player1, player2
+
+    else:
+        player1_name = str(input("Enter your name: "))
+        player2_name = random.choice(
+            ["Alex (bot)", "Nadia (bot)", "Alice (bot)", "John (bot)"])
+
+        player1 = Player(player1_name, False)
+        player2 = Player(player2_name, True)
+        return player1, player2
+
+
+def init_game(player1, player2):
+    """Init game by creating scoreboard and dice objects."""
+    scoreboard = Scoreboard([player1.get_name(), player2.get_name()])
+    dice = Dice()
+    return scoreboard, dice
+
+
+def end_game(scoreboard):
+    """End game and show winner."""
+    if scoreboard.get_winner():
+        terminal.display_clear()
+        terminal.display_winner(scoreboard.get_winner())
+        terminal.display_table(scoreboard.get_scores())
 
 
 if __name__ == '__main__':
